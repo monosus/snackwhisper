6b2a2c2300c837954a455a63b796b3b45bbb05fe
Daisuke Motohashi <ddssk.m@gmail.com>
2024-05-19 07:09:25 +0900

    version番号を指定

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
+import _version
+
+__version__ = _version.__version__
+
 # from tkinter import ttk
 from transcription_app import TranscriptionApp
 from tkinterdnd2 import TkinterDnD
 
-# バージョン番号を指定
-__version__ = "0.1.1"
 
 # ウィンドウの作成とアプリケーションの開始
 window = TkinterDnD.Tk()
