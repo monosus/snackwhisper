417e3f4381b1f0267037309deed0e0b08f0560f0
Daisuke Motohashi <ddssk.m@gmail.com>
2024-06-11 13:19:08 +0900

    長時間の音声文字起こしでエラーが発生して停止する不具合を修正

--- lib/whisper_caller.py
+++ lib/whisper_caller.py
@@ -3,7 +3,22 @@ import os
 import subprocess
 import sys
 import tempfile
+from types import SimpleNamespace
+from lib.debug_options import DebugOptions
 from openai import OpenAI
+
+
+class Transcription:
+    """文字起こし結果を記録するためのクラス"""
+
+    def __init__(self):
+        self.transcription = ""
+        self.last_timestamp_sec = 0
+
+    def add_transcription(self, text: str, last_timestamp_sec: int):
+        """文字起こし結果を追加する"""
+        self.transcription += text
+        self.last_timestamp_sec = last_timestamp_sec
 
 
 class WhisperTranscriptionCaller:
@@ -11,7 +26,7 @@ class WhisperTranscriptionCaller:
         self.api_key = api_key
         self.timestamp_flag = timestamp_flag
 
-        self.transcription = ""
+        self.transcription = Transcription()
         self.language = "ja"
         self.model = "whisper-1"
         self.prompt = """dictionaryを使って、音声を書き起こしてください。
@@ -21,6 +36,18 @@ class WhisperTranscriptionCaller:
 """
 
         # self.client = OpenAI(api_key=api_key)
+
+    def set_options(self, options: DebugOptions):
+        self.debug_options = options
+
+        # 音声ファイルを分割する秒数（0のときは内部で算出する）
+        self.split_segment_sec = options.split_segment_sec
+
+        # ドライランフラグ
+        self.dry_run = options.dry_run
+
+        # コンソール出力
+        self.console_out = options.console_out
 
     def set_prompt(self, prompt: str):
         if prompt is not None:
@@ -38,8 +65,9 @@ class WhisperTranscriptionCaller:
             # # # Split audio if the file size is over the limit
             if sys.flags.debug:
                 print("==== split audio file")
-            if os.path.getsize(audio_file) > 20 * 1024 * 1024:
-                cropped_files = self.split_audio(audio_file, 5 * 1024 * 1024)
+
+            if self.split_segment_sec > 0 or os.path.getsize(audio_file) > 20 * 1024 * 1024:    # 20MB以上のとき
+                cropped_files = self.split_audio(audio_file, 5 * 1024 * 1024)   # 5MBごとに分割
             else:
                 cropped_files = [audio_file]
 
@@ -47,50 +75,86 @@ class WhisperTranscriptionCaller:
                 print(cropped_files)
 
             for cropped_file in cropped_files:
-                self.transcription += self.transcribe_single_file(cropped_file)
+                # self.transcription += self.transcribe_single_file(cropped_file)
+                self.transcribe_single_file(cropped_file)
 
         return self.transcription
 
     def transcribe_single_file(self, audio_file):
+        if self.console_out:
+            print('transcribe_single_file(): ' + audio_file)
+        
         with open(str(audio_file), "rb") as f:
             if self.timestamp_flag:
-                transcript = self.create_with_timestamp(f)
+                self.create_with_timestamp(f)
             else:
-                transcript = self.create(f)
+                self.create(f)
 
-            if sys.flags.debug:
-                print(transcript)
-            return transcript
+            if self.console_out:
+                print(self.transcription.transcription)
+
+            return self.transcription.transcription
 
     def create_with_timestamp(self, file_handler):
-        print(self.prompt)
+        if self.dry_run:
+            print('self.create_with_timestamp(): ')
+            transcript = SimpleNamespace()
+            transcript.segments = [
+                {"start": 0, "end": 5, "text": "これはテストです。"},
+                {"start": 5, "end": 10, "text": "これはテストです。"},
+                {"start": 10, "end": 15, "text": "これはテストです。"},
+            ]
+        else:
+            transcript = self.client.audio.transcriptions.create(
+                model=self.model,
+                file=file_handler,
+                language=self.language,
+                response_format="verbose_json",
+                prompt=self.prompt,
+            )
 
-        transcript = self.client.audio.transcriptions.create(
-            model=self.model,
-            file=file_handler,
-            language=self.language,
-            response_format="verbose_json",
-            prompt=self.prompt,
-        )
+        file_start_sec = self.transcription.last_timestamp_sec
+
+        if self.console_out:
+            print('segments:' + str(transcript))
 
         result = ""
+        begin_sec = self.transcription.last_timestamp_sec
+        last_sec = 0
         for segment in transcript.segments:  # type: ignore
+            if self.console_out:
+                print('create_with_timestamp(): ' + str(segment))
+
             text = segment["text"]
-            endsec = int(segment["start"])
+            endsec = file_start_sec + int(segment["start"])
+            last_sec = int(segment["end"])
             timestamp = str(datetime.timedelta(seconds=endsec))
             result += f"[{timestamp}] {text}\n"
+
+        self.transcription.add_transcription(result, begin_sec + last_sec)
 
         return result
 
     def create(self, file_handler):
-        transcript = self.client.audio.transcriptions.create(
-            model=self.model,
-            file=file_handler,
-            language=self.language,
-            response_format="json",
-        )
-        return transcript.text
 
+        if self.dry_run:
+            print('self.create_with_timestamp(): ')
+            transcript = {
+                "text": "これはテストです。"
+            }
+        else:
+            transcript = self.client.audio.transcriptions.create(
+                model=self.model,
+                file=file_handler,
+                language=self.language,
+                response_format="json",
+            )
+
+        self.transcription.add_transcription(transcript.text, 0)  # type: ignore
+        return transcript.text  # type: ignore
+
+    # ffmpegを使ってファイルを分割する
+    # max_size: 分割するファイルの最大サイズ（Byte）
     def split_audio(self, input_file, max_size):
         startupinfo = None
         if os.name == "nt":  # Windowsの場合
@@ -98,29 +162,51 @@ class WhisperTranscriptionCaller:
             startupinfo.dwFlags |= subprocess.STARTF_USESHOWWINDOW
             startupinfo.wShowWindow = subprocess.SW_HIDE
 
-        duration_command = [
-            "ffprobe",
-            "-i",
-            input_file,
-            "-show_entries",
-            "format=duration",
-            "-v",
-            "quiet",
-            "-of",
-            "csv=p=0",
-        ]
-        duration = float(
-            subprocess.check_output(
-                duration_command,
-                creationflags=subprocess.CREATE_NO_WINDOW,
-                startupinfo=startupinfo,
-            )
-            .decode("utf-8")
-            .strip()
-        )
+        target_duration = self.split_segment_sec
 
-        size = os.path.getsize(input_file)
-        target_duration = (duration * max_size) // size
+        if self.dry_run:
+            target_duration = 5
+        elif target_duration == 0:
+            duration_command = [
+                "ffprobe",
+                "-i",
+                input_file,
+                "-show_entries",
+                "format=duration",
+                "-v",
+                "quiet",
+                "-of",
+                "csv=p=0",
+            ]
+            duration = float(
+                subprocess.check_output(
+                    duration_command,
+                    creationflags=subprocess.CREATE_NO_WINDOW,
+                    startupinfo=startupinfo,
+                )
+                .decode("utf-8")
+                .strip()
+            )
+
+            size = os.path.getsize(input_file)
+            # 音声ファイルのサイズに基づいて、分割するセグメントの目標時間を計算する
+            # duration: 音声ファイルの全体の長さ（秒）
+            # max_size: 分割するファイルの最大サイズ（Byte）
+            # size: 音声ファイルのサイズ（バイト）
+            # target_duration: 分割するセグメントの目標時間（秒）
+            target_duration = (duration * max_size) // size
+
+        # テンポラリディレクトリを作成
+        temp_dir = tempfile.mkdtemp(prefix="splitaudio_")
+        # input_path = os.path.dirname(input_file)
+        output_workpath = os.path.join(temp_dir, "work")
+        os.makedirs(output_workpath, exist_ok=True)
+        output_filepath = os.path.join(output_workpath, "split-%03d.mp3")
+
+        if self.console_out:
+            print("==== split audio file: " + input_file)
+            print("==== target_duration: " + str(target_duration))
+            print("==== output_filepath: " + output_filepath)
 
         command = [
             "ffmpeg",
@@ -132,21 +218,26 @@ class WhisperTranscriptionCaller:
             str(target_duration),
             "-acodec",
             "copy",
-            "work/split-%03d.mp3",
+            str(output_filepath),
             "-loglevel",
             "quiet",
         ]
-        subprocess.run(
-            command,
-            check=True,
-            creationflags=subprocess.CREATE_NO_WINDOW,
-            startupinfo=startupinfo,
-        )
+
+        if self.dry_run:
+            print(" ".join(command))
+            return []
+        else:
+            subprocess.run(
+                command,
+                check=True,
+                creationflags=subprocess.CREATE_NO_WINDOW,
+                startupinfo=startupinfo,
+            )
 
         split_files = []
-        for filename in os.listdir("work"):
+        for filename in os.listdir(output_workpath):
             if filename.startswith("split") and filename.endswith(".mp3"):
-                split_files.append(os.path.join("work", filename))
+                split_files.append(os.path.join(output_workpath, filename))
 
         return sorted(split_files)
 
--- transcription_app.py
+++ transcription_app.py
@@ -6,6 +6,7 @@ import subprocess
 import sys
 
 import tkinter as tk
+from lib.debug_options import DebugOptions
 from lib.status_bar import StatusBar
 
 # from tkinter import ttk
@@ -62,6 +63,9 @@ class TranscriptionApp:
             "DEFAULT", "keep_silence_removed", fallback="False"
         )
         self.keep_silence_removed: bool = setting_keep_silence_removed == "True"
+
+        # デバッグ用のフラグをconfig.iniから読み込む
+        self.debug_options = DebugOptions(self.config)
 
         self.create_widgets()
         self.window.protocol("WM_DELETE_WINDOW", self.on_closing)
@@ -315,13 +319,13 @@ class TranscriptionApp:
         self.load_from_widgets()
 
         # ファイルパスを取得
-        file_path_display_content = self.file_path_display.get("1.0", tk.END)
-        file_path = file_path_display_content.strip()
+        file_path = self.get_filepath()
         timestamp = self.timestamp_flag.get()
 
         # TranscriptionControllerを作成
         controller = self.make_transcription_controller(file_path, timestamp)
         controller.result_encoding = self.result_encoding
+        controller.set_debug_options(self.debug_options)
 
         # APIトークンの有効性を確認
         if controller.check_api_token() is False:
@@ -334,6 +338,11 @@ class TranscriptionApp:
 
         # 音声書き起こしを実行
         controller.transcribe_audio(self.flag_silence_removal)
+
+    def get_filepath(self):
+        file_path_display_content = self.file_path_display.get("1.0", tk.END)
+        file_path = file_path_display_content.strip()
+        return file_path
 
     # TranscriptionControllerを作成
     def make_transcription_controller(self, file_path, timestamp):
@@ -366,6 +375,14 @@ class TranscriptionApp:
             self.set_status("😫 APIトークンが無効です")
         else:
             self.set_status(f"😫 エラーです: {message}")
+            if self.debug_options.export_errorlog:
+                file_path = self.get_filepath()
+                TranscriptionController.output(
+                    file_path,
+                    transcription=message,
+                    encoding=self.result_encoding,
+                    postfix="_errorlog",
+                )
 
     # UIの情報を読み込む
     def load_from_widgets(self):
