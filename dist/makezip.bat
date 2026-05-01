b13ff9e2dbad585f6db8c9b3cb589911f8fb7ce3
Daisuke Motohashi <ddssk.m@gmail.com>
2026-05-01 13:51:51 +0900

    docs: makezip.batの使い方に関するコメントを修正

--- dist/makezip.bat
+++ dist/makezip.bat
@@ -1,5 +1,6 @@
 @REM # CHANGELOG.mdとsnackwhisper.exe を一つのアーカイブファイルにまとめる
-@REM # 使い方: powershell -File makezip.ps1
+@REM # 使い方: cmd.exeから、 
+@REM    $ powershell -File makezip.ps1
 @REM # 依存: 7zip, PowerShell
 
 powershell Compress-Archive -Path CHANGELOG.md,snackwhisper.exe -DestinationPath snackwhisper.zip
